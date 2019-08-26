2b27f65d5d150215c9b5a4f92558b0b8a24b1e16
赵军 <229318442@qq.com>
2019-08-26 13:19:07 +0800

    log4net，添加输出类 LogHelper

---
+++ Common/ApiException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class ApiException : Exception
+    {
+        public int ReturnCode = 1;
+        public ApiException(string msg, int code = 1) : base(msg)
+        {
+            ReturnCode = code;
+        }
+    }
+}
